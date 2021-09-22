154284e148dfd57cce22c1cbc55d0965e5cffeca
Your Name <you@example.com>
2021-09-22 12:41:43 +0000

    delete images

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
 export const fetchCategories = async () => {
-  const response = await axios.get("http://localhost:1337/categories");
+  const response = await axios.get.env(
+    `${process.env.VUE_APP_API_URL}/categories`
+  );
   return response.data;
 };
 
 export const fetchHeroes = async () => {
-  const response = await axios.get("http://localhost:1337/Heroes");
+  const response = await axios.get(`${process.env.VUE_APP_API_URL}/Heroes`);
   return response.data;
 };
 
 export const fetchHomeSections = async () => {
-  const response = await axios.get("http://localhost:1337/Sections");
+  const response = await axios.get(`${process.env.VUE_APP_API_URL}/Sections`);
   return response.data;
 };
